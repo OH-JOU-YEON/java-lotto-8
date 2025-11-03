55c8189d084a3c474d48eff1a8c818b2777177f5
OHJOUYEON <hyperclutter@gmail.com>
2025-11-03 20:53:00 +0900

    test : add validateLottoWinTest

--- src/test/java/lotto/model/domain/LottoTest.java
+++ src/test/java/lotto/model/domain/LottoTest.java
@@ -3,6 +3,7 @@ package lotto.model.domain;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,5 +19,17 @@ class LottoTest {
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 몇 등에 당첨됐는지 테스트")
+    @Test
+    public void validateLottoWinTest() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        Lotto lotto = new Lotto(numbers);
+        int bonusNumber = 7;
+        int userBonusNumber = 8;
+
+        Assertions.assertThat(lotto.validateWin(numbers, bonusNumber, userBonusNumber))
+                .isEqualTo(1);
     }
 }
